1de461995e05a16fa93c4f72daf699631925a59c
Yablonev <68510481+Yablonev@users.noreply.github.com>
2023-11-24 15:49:33 +0000

    Update transferTon.js

--- _modules/transferTon.js
+++ _modules/transferTon.js
@@ -1,13 +1,12 @@
 import { tonweb, TonWeb, tonMnemonic } from '../private/tonweb.js'
 
-export async function transferTon({ mnemonic, version = 'v4R2', address: toAddress, amount: amountTON, payload = '' }) {
+export async function transferTon({ mnemonic, version = 'v4R2', address: toAddress, amount: amountTON, payload = '', sendMode = 3 }) {
   const keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonic)
   const { publicKey, secretKey } = keyPair
   const WalletClass = tonweb.wallet.all[version]
   const wallet = new WalletClass(tonweb.provider, { publicKey, wc: 0 })
   const seqno = (await wallet.methods.seqno().call()) || 0
   const amount = TonWeb.utils.toNano(String(amountTON))
-  const sendMode = 3
   const tx = { secretKey, toAddress, amount, seqno, payload, sendMode }
   try {
     return await wallet.methods.transfer(tx).send()
