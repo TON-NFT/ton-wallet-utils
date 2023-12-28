1122c5fd0644372837f857a4252f96d1c6d47308
notvxn <xanonimikx@gmail.com>
2023-12-28 20:23:00 +0200

    fix: migration to v2

--- private/config.js
+++ private/config.js
@@ -14,8 +14,8 @@ export const VERSION_TYPES = {
   v3R2: 'v3R2',
 }
 
-export const TON_API_KEY = `eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGcwOUY5Il0sImV4cCI6MTgxNjIwMjE3NCwiaXNzIjoiQHRvbmFwaV9ib3QiLCJqdGkiOiI1VTU1VjdVVEUzQk1OMzdYVk1EWVYzSUYiLCJzY29wZSI6InNlcnZlciIsInN1YiI6InRvbmFwaSJ9.tDG3J0wdqIYs1P227m2YeaMk_18CrOw7mE1TYBQ7wVTCCz9WVdx7OI4zX9bL7EvlzdrEzWOG9qma1RpC4V_oCQ`
-export const TON_API_URL = 'https://tonapi.io/v2/blockchain/getAccount?'
+export const TON_API_KEY = `AGDNDGKFQ5V6F4YAAAAF5E4MCKYUIB5J6G2NHDLCCD64Z36GLBSYEB35OQ7N76MSHTPG4YA`
+export const TON_API_URL = 'https://tonapi.io/v2/blockchain/'
 export const NNTN = 1000000000
 export const WALLET_LENGTH = 48
 
